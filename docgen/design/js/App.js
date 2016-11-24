32fea110cbcb701df97a2c2965514311b0d3d99b
vvo <vincent.voyer@algolia.com>
2016-11-24 11:34:25 +0100

    show how to set pagination default

--- docgen/design/js/App.js
+++ docgen/design/js/App.js
@@ -51,6 +51,7 @@ const Variation = props =>
   appId="latency"
   apiKey="6be0576ff61c053d5f9a3225e2a90f76"
   indexName="ikea"
+  {...props}
 >
   <div>
     {props.children}
@@ -157,7 +158,17 @@ const Facets = () =>
   <hr/>
 
   <h2>Pagination</h2>
-  <Pagination />
+  <Variation />
+    <Pagination />
+  <Variation />
+  <hr/>
+
+  <h2>Pagination page 5 selected</h2>
+  <Variation />
+    <Pagination
+      defaultRefinement={5}
+    />
+  <Variation />
   <hr/>
 
   <h2>PoweredBy</h2>
